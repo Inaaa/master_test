2844ca11c8350e6147632e1076ef3885b4c0db37
Inaaa <chanchan.li@outlook.com>
2020-04-11 09:15:21 +0200

    add projection

--- temp.py
+++ temp.py
@@ -1,19 +1,10 @@
 import numpy as np
-pts_3d = lidar
-n = pts_3d.shape[0]
-pts_3d_hom = np.hstack((pts_3d, np.ones((n, 1))))
-pts_3d_velo = pts_3d_hom
-pts_3d_ref = np.dot(pts_3d_velo, np.transpose(self.V2C))   # velo to ref
-pts_3d_rect = np.transpose(np.dot(self.R0, np.transpose(pts_3d_ref)))   # ref to rect
+import matplotlib.pyplot as plt
 
-pts_3d = pts_3d_rect
 
-n = pts_3d.shape[0]
-pts_3d_hom = np.hstack((pts_3d, np.ones((n, 1))))
+position = np.loadtxt('/mrtstorage/users/chli/real_data/1_lidar_pos.txt')
+x = position[:,0]
+y = position[:,1]
 
-pts_3d_rect=pts_3d_hom
-
-pts_2d = np.dot(pts_3d_rect, np.transpose(self.P))  # nx3
-pts_2d[:, 0] /= pts_2d[:, 2]
-pts_2d[:, 1] /= pts_2d[:, 2]
-result = pts_2d[:, 0:2]
+plt.scatter(x, y, alpha=0.6)
+plt.show()
